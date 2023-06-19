47b3fb5cb5ce2df37443fe8aa1e496b2a9dcb19b
Haris <developewithharis@gmail.com>
2023-06-19 17:10:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// let previewContainer = document.querySelector(".products-preview");
+// let previewBox = previewContainer.querySelector(".preview");
+
+// document.querySelectorAll(".products-container .product").forEach(product => {
+//     product.onclick = () =>{
+//         let name = product.getAttribute("data-name");
+//         previewBox.forEach(preview => {
+//         let target = preview.getAttribute("data-target");
+//         if(name == target) {
+//             preview.classList.add("active")
+//         }
+//         })
+
+//     };
+
+// });
